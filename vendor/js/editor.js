f13e42b901bade520e10d838e7eb6aa5cf328e10
历风 <lb1064@qq.com>
2015-06-02 11:22:41 +0800

    拖拽处理边界和handler resize处理max和min

--- vendor/js/editor.js
+++ vendor/js/editor.js
@@ -2,13 +2,13 @@ require.config({
     paths: {
         jquery: '/lib/jqueryui/external/jquery/jquery',
         jqui: '/lib/jqueryui/jquery-ui',
-        btncom:'/js/module/btncom/btncom',
-        btncom_content:'/js/module/btncom/btncom_content',
-        btncom_style:'/js/module/btncom/btncom_style',
-        imgcom:'/js/module/imgcom/imgcom',
-        imgcom_content:'/js/module/imgcom/imgcom_content',
-        imgcom_style:'/js/module/imgcom/imgcom_style',
-        page:'/js/module/page/page'
+        btncom: '/js/module/btncom/btncom',
+        btncom_content: '/js/module/btncom/btncom_content',
+        btncom_style: '/js/module/btncom/btncom_style',
+        imgcom: '/js/module/imgcom/imgcom',
+        imgcom_content: '/js/module/imgcom/imgcom_content',
+        imgcom_style: '/js/module/imgcom/imgcom_style',
+        page: '/js/module/page/page'
     },
     shim: {
         'jqui': {
@@ -17,60 +17,72 @@ require.config({
     }
 });
 
-require(['zepto','jquery','btncom', 'imgcom', 
-    'btncom_content','btncom_style','imgcom_content',
-    'imgcom_style','jqui','page'], function (zepto,$,Btncom,Imgcom,btncom_content,btncom_style,imgcom_content,imgcom_style,jqui,Page) {
+require(['zepto', 'jquery', 'btncom', 'imgcom',
+    'btncom_content', 'btncom_style', 'imgcom_content',
+    'imgcom_style', 'jqui', 'page'], function (zepto, $, Btncom, Imgcom, btncom_content, btncom_style, imgcom_content, imgcom_style, jqui, Page) {
 
 
-        var Btncom = Btncom.Btncom;
-        var Imgcom = Imgcom.Imgcom;
-        $('#prototype-content').tabs();
-        var page = new Page();
-            page.initPage({Btncom:Btncom});
+    var Btncom = Btncom.Btncom;
+    var Imgcom = Imgcom.Imgcom;
+    $('#prototype-content').tabs();
+    var page = new Page();
+    page.initPage({Btncom: Btncom});
 
 
-        var addtext = $('#addtext');
-        var addimage = $('#addimage');
-        var addbutton = $('#addbutton');
-        var addpages = $('.add-page-list');
+    var addtext = $('#addtext');
+    var addimage = $('#addimage');
+    var addbutton = $('#addbutton');
+    var addpages = $('.add-page-list');
 
-        addpages.on('click',function(){
-            page.addPage();
-        });
-        addimage.on('click',function(){
-            var imgcom = new Imgcom();
-            imgcom.render({
-                container:zepto('#showbox')
-            });
-            
-        });
-        addbutton.on('click',function(){
-            var pageId = page.getSelectPage();
-            var btncom = new Btncom({
-                pageId:pageId
-            });
-            btncom.render({
-                container:zepto('#showbox')
-            });
+    addpages.on('click', function () {
+        page.addPage();
+    });
+    addimage.on('click', function () {
+        var imgcom = new Imgcom();
+        imgcom.render({
+            container: zepto('#showbox')
         });
 
+    });
+    addbutton.on('click', function () {
+        var pageId = page.getSelectPage();
+        var btncom = new Btncom({
+            pageId: pageId
+        });
+        btncom.render({
+            container: zepto('#showbox')
+        });
+    });
 
-    $('#showbox').on('click','.W_iteam',function(){
+
+    $('#showbox').on('click', '.W_iteam', function () {
         var $that = $(this);
         var siblings = $that.siblings();
         $that.addClass('select');
-        $that.draggable();
+        $that.draggable({
+            cursor: 'move',
+            containment: 'parent',
+            cancel: false
+        });
         $that.resizable({
-            handles:' n, e, s, w, ne, se, sw, nw'
+            handles: ' n, e, s, w, ne, se, sw, nw',
+            maxWidth:300,
+            minWidth:50,
+            maxHeight:50,
+            minHeight:20
         });
         siblings.removeClass('select');
-        siblings.each(function(i,o){
-            if($(o).resizable( "instance" )){
-                $(o).resizable( "destroy" );
+        siblings.each(function (i, o) {
+            if ($(o).resizable('instance')) {
+                $(o).resizable('destroy');
             }
+            if ($(o).draggable('instance')) {
+                $(o).draggable('destroy');
+            }
+
         });
 
     });
 
-        
+
 });
