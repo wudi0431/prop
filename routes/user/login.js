5926834c9ece58eb8213c5b3670f5b9a489e097f
wudi <wudi2852987@163.com>
2015-06-30 17:23:25 +0800

    update project

--- routes/user/login.js
+++ routes/user/login.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 var filter = require('../../filter/filter')
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     if (req.session.user) {
         res.redirect('/');
     } else {
         res.render('login');
     }
 });
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
     filter.login(req, res, next);
 });
 module.exports = router;
