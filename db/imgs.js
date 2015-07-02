f9109c19386484f44ea1fcf191042021c78d6f90
历风 <lb1064@qq.com>
2015-07-02 11:27:12 +0800

    passport

--- db/imgs.js
+++ db/imgs.js
@@ -18,10 +18,7 @@ ImgsSchema.static('getImgsByUser', function (user, cb) {
 });
 
 ImgsSchema.static('getPubImgs', function (cb) {
-    return this.find({}).populate({
-        path: 'user',
-        select: 'name'
-    }).exec(function (err, obj) {
+    return this.find({}).exec(function (err, obj) {
         if(obj){
             obj = obj.filter(function (o) {
                 return o.user.name === 'admin';
