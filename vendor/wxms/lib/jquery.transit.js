b6ff19a778c858ac6ec08a7a02beb495d7c41832
wudi <wudi2852987@163.com>
2015-07-07 17:15:14 +0800

    update all src

--- vendor/wxms/lib/jquery.transit.js
+++ vendor/wxms/lib/jquery.transit.js
@@ -12,7 +12,7 @@
 ;(function (root, factory) {
 
   if (typeof define === 'function' && define.amd) {
-    define(['jquery'], factory);
+    define(['jquery.transit'], factory);
   } else if (typeof exports === 'object') {
     module.exports = factory(require('jquery'));
   } else {
