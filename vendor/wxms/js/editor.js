e1c0fa13fbfac1da7957fef58389990acc545f59
历风 <lb1064@qq.com>
2015-07-09 10:19:19 +0800

    暂时屏蔽预览

--- vendor/wxms/js/editor.js
+++ vendor/wxms/js/editor.js
@@ -77,7 +77,7 @@ require(['wxms_config','template', 'rotatable', 'html2canvas', 'zepto', 'jquery'
         var projectId = getQueryString("projectId");
         var j_preview_app = $('.j_preview_app');
         j_preview_app.on('click', function () {
-            window.open(WXMS_config.domain+'/preview?projectId=' + projectId);
+            //window.open(WXMS_config.domain+'/preview?projectId=' + projectId);
         });
 
         var j_saveTpl = $('.j_saveTpl');
