717ff0d46781a31f3a8e840e19b5367654526fb8
wudi <wudi2852987@163.com>
2015-07-14 14:51:28 +0800

    update logo

--- config.js
+++ config.js
@@ -1,19 +1,22 @@
 var config = {
-    port:9898,
+    port: 9898,
     passport: 'http://yhd.adanghome.com/passport',
-    domain:'http://yhd.adanghome.com/wxms',
-    previewDomain:'http://mxc.yhd.com/wxms_client'
+    domain: 'http://yhd.adanghome.com/wxms',
+    previewDomain: 'http://mxc.yhd.com/wxms_client'
 };
 
 //var config = {
-//    port:9898,
-//    passport: 'http://120.132.50.71/passport'
+//    port: 9898,
+//    passport: 'http://yhd.adanghome.com/passport',
+//    previewDomain: 'http://mxc.yhd.com/wxms_client'
 //};
 
-if ( typeof module === "object" && typeof module.exports === "object" ) {
+if (typeof module === "object" && typeof module.exports === "object") {
     module.exports = config;
 } else {
-    if ( typeof define === "function") {
-        define( "wxms_config", [], function () { return config; } );
+    if (typeof define === "function") {
+        define("wxms_config", [], function () {
+            return config;
+        });
     }
 }
