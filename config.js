983213e1079b0ab7ce811b7f1eaeb63d805b4e75
历风 <lb1064@qq.com>
2015-07-14 09:24:58 +0800

    微信分享

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {
     port:9898,
     passport: 'http://120.132.50.71/passport',
     domain:'http://120.132.50.71/wxms',
-    previewDomain:'http://120.132.50.71/wx'
+    previewDomain:'http://mxc.yhd.com/wxms_client'
 };
 
 //var config = {
