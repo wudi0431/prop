f12a8f8ec75bdf760cf4e28ffd409ba72b21b2e3
历风 <lb1064@qq.com>
2015-08-24 16:25:51 +0800

    增加获取音乐的jsonp接口

--- routes/audiocom/getAudiocomListByprojectId.js
+++ routes/audiocom/getAudiocomListByprojectId.js
@@ -3,28 +3,48 @@ var router = express.Router();
 var filter = require('../passport.js');
 var Audiocom = require('../../db/audiocom');
 router.get('/', function (req, res, next) {
-    filter.authorize(req, res, function (req, res) {
-        var projectId = req.query.projectId;
-        Audiocom.getAudiocomListByprojectId(projectId, function (err, audiocom) {
-            if (err) {
-                res.status('500');
-                res.send({
-                    success: false, // 标记失败
-                    model: {
-                        error: '系统错误'
-                    }
-                });
-            } else {
-                res.status('200');
-                res.send({
-                    success: true,
-                    model: audiocom
-                });
-            }
+  filter.authorize(req, res, function (req, res) {
+    var projectId = req.query.projectId;
+    Audiocom.getAudiocomListByprojectId(projectId, function (err, audiocom) {
+      if (err) {
+        res.status('500');
+        res.send({
+          success: false, // 标记失败
+          model: {
+            error: '系统错误'
+          }
         });
-
-
+      } else {
+        res.status('200');
+        res.send({
+          success: true,
+          model: audiocom
+        });
+      }
     });
+
+
+  });
+
+});
+
+router.get('/jsonp', function (req, res, next) {
+  var projectId = req.query.projectId;
+  Audiocom.getAudiocomListByprojectId(projectId, function (err, audiocom) {
+    if (err) {
+      res.jsonp({
+        success: false,
+        model: {
+          error: '系统错误'
+        }
+      });
+    } else {
+      res.jsonp({
+        success: true,
+        model: audiocom
+      });
+    }
+  });
 
 });
 
