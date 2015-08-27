ba38f317f75bf4c1c02a2d0fc1d45e43f1f92970
ZhangJingJing <1067133138@qq.com>
2015-08-27 10:55:10 +0800

    config.js

--- config.js
+++ config.js
@@ -1,17 +1,10 @@
-//var config = {
-//    port:9898,
-//    passport: 'http://yhd.adanghome.com/passport',
-//    domain:'http://120.132.50.71/wxms',
-//    previewDomain:'http://mxc.yhd.com/wxms_client'
-//};
-
 var config = {
-    port:9898,
-    passport: 'http://yhd.adanghome.com/passport',
-    domain:'http://127.0.0.1:9898'
+   port:9898,
+   passport: 'http://yhd.adanghome.com/passport',
+   domain:'http://120.132.50.71/wxms',
+   previewDomain:'http://mxc.yhd.com/wxms_client'
 };
 
-<<<<<<< HEAD
 // var config = {
 //    port: 9898,
 //    passport: 'http://yhd.adanghome.com/passport',
@@ -19,8 +12,6 @@ var config = {
 //    previewDomain: 'http://mxc.yhd.com/wxms_client'
 // };
 
-=======
->>>>>>> c0d7e1e5f1336a53182ba33319e3a8a85ee7f0ad
 if ( typeof module === "object" && typeof module.exports === "object" ) {
     module.exports = config;
 } else {
