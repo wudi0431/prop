299148283f3636ed3cb953f11dffde52ad1e1f17
wudi <wudi2852987@163.com>
2015-08-27 12:53:35 +0800

    fix

--- config.js
+++ config.js
@@ -5,22 +5,14 @@
 //    previewDomain:'http://mxc.yhd.com/wxms_client'
 //};
 
-var config = {
-    port:9898,
+ var config = {
+    port: 9898,
     passport: 'http://yhd.adanghome.com/passport',
-    domain:'http://127.0.0.1:9898'
-};
+     domain:'http://127.0.0.1:9898',
+    previewDomain: 'http://mxc.yhd.com/wxms_client'
+ };
 
-<<<<<<< HEAD
-// var config = {
-//    port: 9898,
-//    passport: 'http://yhd.adanghome.com/passport',
-//     domain:'http://127.0.0.1:9898',
-//    previewDomain: 'http://mxc.yhd.com/wxms_client'
-// };
 
-=======
->>>>>>> c0d7e1e5f1336a53182ba33319e3a8a85ee7f0ad
 if ( typeof module === "object" && typeof module.exports === "object" ) {
     module.exports = config;
 } else {
