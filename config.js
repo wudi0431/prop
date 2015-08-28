ddfe8e6e2ae209cdd582c5fee24e025e4c0549bb
zhoubenbentime <admin@zhouxianbao.cn>
2015-08-28 13:55:26 +0800

    Signed-off-by: zhoubenbentime <admin@zhouxianbao.cn>

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
-var config = {
+/*var config = {
     port:9898,
     passport: 'http://yhd.adanghome.com/passport',
     domain:'http://120.132.50.71/wxms',
     previewDomain:'http://mxc.yhd.com/wxms_client'
-};
+};*/
 
-//var config = {
-//    port: 9898,
-//    passport: 'http://yhd.adanghome.com/passport',
-//    previewDomain: 'http://mxc.yhd.com/wxms_client'
-//};
+var config = {
+    port: 9898,
+    passport: 'http://yhd.adanghome.com/passport',
+    previewDomain: 'http://mxc.yhd.com/wxms_client',
+  domain:'http://127.0.0.1'
+};
 
 if ( typeof module === "object" && typeof module.exports === "object" ) {
     module.exports = config;
