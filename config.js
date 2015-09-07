2b2bf1b69ddfb11606266ab62d908b957ead0068
wudi <wudi2852987@163.com>
2015-09-07 10:33:44 +0800

    config

---
+++ config.js
@@ -0,0 +1,21 @@
+//var config = {
+//   port:9898,
+//   passport: 'http://yhd.adanghome.com/passport',
+//   domain:'http://120.132.50.71/wxms',
+//   previewDomain:'http://mxc.yhd.com/wxms_client'
+//};
+
+ var config = {
+    port: 9898,
+    passport: 'http://yhd.adanghome.com/passport',
+     domain:'http://127.0.0.1:9898',
+    previewDomain: 'http://mxc.yhd.com/wxms_client'
+ };
+
+if ( typeof module === "object" && typeof module.exports === "object" ) {
+    module.exports = config;
+} else {
+    if ( typeof define === "function") {
+        define( "wxms_config", [], function () { return config; } );
+    }
+}
