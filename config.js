98e2ee07584f913a3272599172ad6facfa97e44b
tom <wudi2852987@163.com>
2015-11-09 11:04:01 +0800

    update config js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@
 
  var config = {
      port:9898,
-     passport: 'http://120.132.50.71/passport',
-     domain:'http://120.132.50.71/wxms',
-     previewDomain:'http://mxc.yhd.com/wxms_client'
+     passport: 'http://10.6.80.207/passport',
+     domain:'http://10.6.80.207:9898',
+previewDomain:'http://10.6.80.207/wx' 
  };
 config.uploadimgsrc = config.domain+'/uploadimg/';
 
